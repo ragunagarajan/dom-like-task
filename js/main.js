ee4181e7a650cb0eed462a68a0d00ab16686b299
ragunagarajan <rpathi783@gmail.com>
2023-05-03 12:10:07 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,58 +1,49 @@
 //function to handle like click event
-	 
-	function handLike(event){
 
-	event.preventDefault();		 
-	//find the i element
-		
-		let iEle=this.getElementsByClassName("fa-heart-o");	
-		 iEle=iEle[0];
-		 
-	//find the span element
-		let spaEle=this.parentNode.getElementsByTagName("span");
-		spaEle=spaEle[0];
-		
-	//find current like count
-		let curreLike=spaEle.innerText;
-		
-		let likecount=parseInt(curreLike);
-		
-	//chech if it is a active class name
-		
-	if(iEle.className.includes("active")){
-		//if yes
-		
-			//reduce the like count
-			likecount=likecount-1;
-			
-			console.log(likecount);
-			//remove the active class
-			iEle.classList.remove("active");
-		
-	}
-	else{
-		//if no
-		
-			//increase the like count
-			likecount=likecount+1;
-			
-			//add the active class
-			iEle.classList.add("active");
-		
-	}
-	//update the span text
-	spaEle.innerText=likecount+" "+"Likes";	
-}	
+function handLike(event) {
+    event.preventDefault();
+    //find the i element
+
+    let iEle = this.getElementsClassName("fa-heart-o");
+    iEle = iEle[0];
+
+    //find the span element
+    let spaEle = this.parentNode.getElementsByTagName("span");
+    spaEle = spaEle[0];
+
+    //find current like count
+    let curreLike = spaEle.innerText;
+
+    let likecount = parseInt(curreLike);
+
+    //chech if it is a active class name
+
+    if (iEle.className.includes("active")) {
+        //if yes
+
+        //reduce the like count
+        likecount = likecount - 1;
+
+        //remove the active class
+        iEle.classList.remove("active");
+    } else {
+        //if no
+
+        //increase the like count
+        likecount = likecount + 1;
+
+        //add the active class
+        iEle.classList.add("active");
+    }
+    //update the span text
+    spaEle.innerText = likecount + " " + "Likes";
+}
 // find a.like all element
-	
-	let likeEle=document.getElementsByClassName("like");
 
+let likeEle = document.getElementsByClassName("like");
 
 //iterate the html collection
-	for(let i=0;i < likeEle.length;i++ ){
-
-	likeEle[i].addEventListener("click",handLike)
-	
-	}		
-   //add event listener for each a.like
-	
+for (let i = 0; i < likeEle.length; i++) {
+    likeEle[i].addEventListener("click", handLike);
+}
+//add event listener for each a.like
